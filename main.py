9b8eb0376f75098adae783ab19b788ad23c9fedd
Liam Watts <35185935+Wattsy2020@users.noreply.github.com>
2018-06-05 13:18:45 +1000

    Be less annoying
    -Bot now ignores meta-discussion (comments with the word 'comment' in them and replies to the bot)
    -Minor fix to handle deleted comments and other Reddit API exceptions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import praw
-import time
 import re
 
 
@@ -11,6 +10,7 @@ def login():
                          user_agent='')
 
     return reddit
+
 
 def convert_word(word):
     word = re.sub('[^a-z]+', '', word)  # get plain text
@@ -25,6 +25,16 @@ def convert_word(word):
           end+" and the "+start+"children"+end+" too!")
 
 
+def is_response_to_bot(comment):
+    if comment.is_root:  # the bot will not make any top level comments
+        return False
+
+    if comment.author == 'InclusiveMemer':
+        return True
+
+    return is_response_to_bot(comment.parent())
+
+
 def main():
     for comment in subreddit.stream.comments():
         # ignore the bots own comments
@@ -33,25 +43,24 @@ def main():
         if 'bot' in str(comment.author).lower(): continue
 
         comment_text = comment.body.lower()
-        if 'men' in comment_text:
+        if 'men' in comment_text and not is_response_to_bot(comment):
             words = comment_text.split(' ')
 
             for word in words:
                 if 'men' in word:
-                    # ignore really long words, most likely links
-                    if len(word) > 20: break
+                    # ignore really long words that are most likely links and meta discussion
+                    if len(word) > 20 or word == 'comment': continue
                     
                     reply_phrase = convert_word(word)
 
                     # if someone already made the same joke don't repeat it
                     if reply_phrase in comment_text: break
                     
-                    # if ratelimited wait and then rety
+                    # handle any api exceptions e.g. comment was deleted, connection timeout
                     try:
                         comment.reply(reply_phrase)
                     except praw.exceptions.APIException:
-                        time.sleep(600)
-                        comment.reply(reply_phrase)
+                        break
 
                     link = 'reddit.com' + comment.submission.permalink + comment.fullname[3:]
                     print('Replied to:           {}'.format(link))
